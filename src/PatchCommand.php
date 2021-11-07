2ce75e4c2efa347651cd2a5daedcdc758c020650
xandros15 <xandros92@gmail.com>
2021-11-07 16:29:07 +0100

    wrap logger to log exceptions

--- src/PatchCommand.php
+++ src/PatchCommand.php
@@ -9,9 +9,9 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Question\Question;
+use throwable;
 use WBCUpdater\Exceptions\FileExistException;
 use WBCUpdater\Exceptions\InputException;
-use WBCUpdater\Exceptions\SystemException;
 use WBCUpdater\Exceptions\UnsupportedArchiveException;
 
 final class PatchCommand extends Command
@@ -36,105 +36,110 @@ final class PatchCommand extends Command
      * @param OutputInterface $output
      *
      * @return int
-     * @throws SystemException
+     * @throws throwable
      */
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        $game = new Game($this->config['game_directory']);
-
-        /** @var $helper QuestionHelper */
-        $helper = $this->getHelper('question');
-        while (!$game->checkDirectory()) {
-            $output->writeln("Cannot find game in {$game->getDirectory()}");
-            $question = new Question('Type correct directory in command line:');
-            $answer = $helper->ask($input, $output, $question);
-            $game->changeDirectory($answer);
-        }
-        //update config
-        $this->config['game_directory'] = $game->getDirectory();
-        $this->config->save();
-
-        //normalize megatools
-        $megatools = $this->config['megatools_exe'];
-        if (substr($megatools, 1, 1) !== ':') {
-            $megatools = $this->config['megatools_exe'];
-        }
-
-        $link = file_get_contents($this->config['patch_server']);
-
-        if (!$link || !preg_match('~^https://mega.nz/file/.+~', $link)) {
-            $question = new Question('Please, provide mega.nz link to patch: ');
-            $question->setValidator(function ($answer) {
-                if (!preg_match('~^https://mega.nz/file/.+~', $answer)) {
-                    throw new InputException('Link should be valid mega link to file.');
-                }
-
-                return $answer;
-            });
-            $link = $helper->ask($input, $output, $question);
-            if (!$link) {
-                $output->writeln('Missing patch link, exit.');
-
-                return Command::INVALID;
-            }
-        }
-
-        //downloader
-        $downloader = new MegaPatchDownloader($link, $megatools, $this->config, $this->logger);
-
         try {
-            $output->writeln(sprintf('Starting download patch from %s', $link));
-            $downloader->download();
-        } catch (FileExistException $exception) {
-            $output->writeln($exception->getMessage());
-            $question = new ConfirmationQuestion('Do you want to override patch file? (y/N): ', false);
-            $answer = $helper->ask($input, $output, $question);
-            if ($answer) {
-                unlink($downloader->getDownloadedFile()->getRealPath());
+            $game = new Game($this->config['game_directory']);
+
+            /** @var $helper QuestionHelper */
+            $helper = $this->getHelper('question');
+            while (!$game->checkDirectory()) {
+                $output->writeln("Cannot find game in {$game->getDirectory()}");
+                $question = new Question('Type correct directory in command line:');
+                $answer = $helper->ask($input, $output, $question);
+                $game->changeDirectory($answer);
+            }
+            //update config
+            $this->config['game_directory'] = $game->getDirectory();
+            $this->config->save();
+
+            //normalize megatools
+            $megatools = $this->config['megatools_exe'];
+            if (substr($megatools, 1, 1) !== ':') {
+                $megatools = $this->config['megatools_exe'];
+            }
+
+            $link = file_get_contents($this->config['patch_server']);
+
+            if (!$link || !preg_match('~^https://mega.nz/file/.+~', $link)) {
+                $question = new Question('Please, provide mega.nz link to patch: ');
+                $question->setValidator(function ($answer) {
+                    if (!preg_match('~^https://mega.nz/file/.+~', $answer)) {
+                        throw new InputException('Link should be valid mega link to file.');
+                    }
+
+                    return $answer;
+                });
+                $link = $helper->ask($input, $output, $question);
+                if (!$link) {
+                    $output->writeln('Missing patch link, exit.');
+
+                    return Command::INVALID;
+                }
+            }
+
+            //downloader
+            $downloader = new MegaPatchDownloader($link, $megatools, $this->config, $this->logger);
+
+            try {
                 $output->writeln(sprintf('Starting download patch from %s', $link));
                 $downloader->download();
+            } catch (FileExistException $exception) {
+                $output->writeln($exception->getMessage());
+                $question = new ConfirmationQuestion('Do you want to override patch file? (y/N): ', false);
+                $answer = $helper->ask($input, $output, $question);
+                if ($answer) {
+                    unlink($downloader->getDownloadedFile()->getRealPath());
+                    $output->writeln(sprintf('Starting download patch from %s', $link));
+                    $downloader->download();
+                }
             }
-        }
 
-        if (!$downloader->getDownloadedFile()) {
-            throw new InputException('Cannot find patch file.');
-        }
+            if (!$downloader->getDownloadedFile()) {
+                throw new InputException('Cannot find patch file.');
+            }
 
-        $gameOverrider = new GameOverrider();
+            $gameOverrider = new GameOverrider();
 
-        $question = new ConfirmationQuestion(
-            'Do you want to override KeyBinds? Warning: tooltip will be not updated. (y/N): ',
-            false
-        );
-        $override = new Override('keybinds', '/^English\\\Help\.cfg$/');
-        $gameOverrider->add($override);
-        if ($helper->ask($input, $output, $question)) {
-            $override->enable();
-        }
+            $question = new ConfirmationQuestion(
+                'Do you want to override KeyBinds? Warning: tooltip will be not updated. (y/N): ',
+                false
+            );
+            $override = new Override('keybinds', '/^English\\\Help\.cfg$/');
+            $gameOverrider->add($override);
+            if ($helper->ask($input, $output, $question)) {
+                $override->enable();
+            }
 
-        $question = new ConfirmationQuestion('Do you want to override Army Sounds? (y/N): ', false);
-        $override = new Override('sounds', '/^Assets\\\Sides\\\\\w+VoicesEn.xcr$/');
-        $gameOverrider->add($override);
-        if ($helper->ask($input, $output, $question)) {
-            $override->enable();
-        }
+            $question = new ConfirmationQuestion('Do you want to override Army Sounds? (y/N): ', false);
+            $override = new Override('sounds', '/^Assets\\\Sides\\\\\w+VoicesEn.xcr$/');
+            $gameOverrider->add($override);
+            if ($helper->ask($input, $output, $question)) {
+                $override->enable();
+            }
 
-        if ($downloader->getDownloadedFile()->getExtension() !== 'rar') {
-            throw new UnsupportedArchiveException("{$downloader->getDownloadedFile()->getFilename()} isn't supported file.");
-        }
+            if ($downloader->getDownloadedFile()->getExtension() !== 'rar') {
+                throw new UnsupportedArchiveException("{$downloader->getDownloadedFile()->getFilename()} isn't supported file.");
+            }
 
-        $builder = new PatchFactory(RarPatch::class);
-        $patch = $builder->create($downloader->getDownloadedFile());
-        $patcher = new GamePatcher(
-            $patch,
-            $game,
-            $gameOverrider,
-            $this->logger
-        );
-        $patcher->patch();
+            $builder = new PatchFactory(RarPatch::class);
+            $patch = $builder->create($downloader->getDownloadedFile());
+            $patcher = new GamePatcher(
+                $patch,
+                $game,
+                $gameOverrider,
+                $this->logger
+            );
+            $patcher->patch();
 
-        if ($game->removePatchXcr()) {
-            $output->writeln('Removed PatchData.xcr.');
+            if ($game->removePatchXcr()) {
+                $output->writeln('Removed PatchData.xcr.');
+            }
+        } catch (throwable $e) {
+            $this->logger->error($e);
+            throw $e;
         }
 
         return Command::SUCCESS;
