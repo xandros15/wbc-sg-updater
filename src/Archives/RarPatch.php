ece77e28b742f3e00c2c64273397316930d5cb78
xandros15 <xandros92@gmail.com>
2022-04-14 20:59:50 +0200

    hot fix phprar error with unicode chars in patch filename

--- src/Archives/RarPatch.php
+++ src/Archives/RarPatch.php
@@ -7,6 +7,7 @@ namespace WBCUpdater\Archives;
 use Generator;
 use RarArchive;
 use SplFileInfo;
+use WBCUpdater\Exceptions\RuntimeException;
 
 final class RarPatch implements PatchInterface
 {
@@ -25,13 +26,35 @@ final class RarPatch implements PatchInterface
      */
     public function getFiles(): Generator
     {
-        $archive = RarArchive::open($this->file->getRealPath());
+        $archive = $this->createArchive();
         foreach ($archive->getEntries() as $entry) {
             if ($entry && !$entry->isDirectory()) {
                 yield new RarFile($entry);
             }
         }
         $archive->close();
+    }
+
+    /**
+     * Workaround https://bugs.php.net/bug.php?id=75557
+     *
+     * @return RarArchive
+     */
+    private function createArchive(): RarArchive
+    {
+        if (preg_match('/[~a-z_\s!@#$%^&()+-=.,{}\[];\']+/i', $this->file->getBasename())) {
+            return RarArchive::open($this->file->getRealPath());
+        }
+
+        $filename = $this->file->getPath() . '/' . uniqid();
+
+        if (!copy($this->file->getRealPath(), $filename)) {
+            throw new RuntimeException('Cannot make an temporary patch file.');
+        }
+
+        register_shutdown_function(fn () => @unlink($filename));
+
+        return RarArchive::open($filename);
     }
 
     /**
